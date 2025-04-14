a3bb679bc7b77ba2d77cd3919168abea541a78f6
SPraveeen <praveenji8498@gmail.com>
2025-04-14 13:20:10 +0530

    adding

--- my-app/src/Task.jsx
+++ my-app/src/Task.jsx
@@ -1,12 +1,9 @@
+import checkbox from "./checkbox";
+
 export default function Task(){
   return (
     <div className="Task">
-    <div className="checkbox unchecked">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"></svg>
-    </div>
-    <div className="checkbox checked">
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"></svg>
-    </div>
+    <checkbox defaultChecked={true}/>
     test task
     </div>
   )
