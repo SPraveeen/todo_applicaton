30982383d0258b30d67dbd985ec81191f09bf8b8
SPraveeen <praveenji8498@gmail.com>
2025-04-15 19:56:40 +0530

    adding

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -22,15 +22,46 @@ function App() {
     });
   }
   
+  function removeTask(indexToRemove){
+    setTasks(prev=>{
+      return prev.filter((taskObject,index)=>index !== indexToRemove);
+    });
+  }
   function updateTaskDone(taskIndex,newDone){
-    
+    setTasks(prev=>{
+      const newTasks = [...prev];
+      newTasks[taskIndex].done = newDone;
+      return newTasks;
+    });
+  }
+  
+    const numberComplete=tasks.filter(t=>t.done).length;
+    const numberTotal=tasks.length;
+
+  function getMessage(){
+    const percentage = numberComplete / numberTotal * 100;
+    if(percentage === 0) {return 'Try to do atleast one';}
+    if(percentage === 100){return 'Nice job for today! 🎉';}
+    return 'Keep it going 💪'
   }
 
+  function renameTask(index,newName){
+    setTasks(prev=>{
+      const newTasks=[...prev];
+      newTasks[index].name=newName;
+      return newTasks;
+    })
+  }
   return (
     <main>
+      <h1>{numberComplete}/{numberTotal} Complete</h1>
+      <h2>{getMessage}</h2>
       <Taskform onAdd={addTask}/>
       {tasks.map((task,index)=>(
-        <Task{...task} onToggle={done=>updateTaskDone(index,done)} />
+        <Task{...task}
+        onRename={newName=>renameTask(index,newName)} 
+        onTrash={()=>removeTask(index)}
+        onToggle={done=>updateTaskDone(index,done)} />
       ))}
     </main>      
   );
